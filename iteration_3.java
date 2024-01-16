bbc96167a1edb0b24d3a7315e3fbec3b5c848936
zieunn <kje003300@naver.com>
2024-01-16 23:46:15 +0900

    반복문 2번 A+B-3 추가

--- iteration_3.java
+++ iteration_3.java
@@ -8,20 +8,28 @@ public class iteration {
                         
         for(int i = 1; i <= 9; i++) {
             System.out.println(n+" * "+i+" = "+n*i);
-                }	                                             
-            
-            
-            
+                }	                                                                  
     }   
 }                                                              */
 
-
+// 2번 A+B-3
 import java.util.Scanner;
 public class iteration_3 {
     public static void main(String[] args) {
-		
-            
-            
+        
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int result[] = new int[t];
+ 
+		for (int i = 0; i < t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			result[i] = a + b;
+		}
+ 
+		for (int i = 0; i < t; i++) {
+			System.out.println(result[i]);
+		}	                        
     }   
 }
   
