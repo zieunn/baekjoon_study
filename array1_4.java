473845584ab21718d4df5fafecedcbdb6eb3f105
zieunn <kje003300@naver.com>
2024-01-24 23:53:20 +0900

    1차원 배열 7번 추가

--- array1_4.java
+++ array1_4.java
@@ -135,7 +135,7 @@ public class array1_4 {
     }
 }           */
 
-// 6 : 공 바꾸기
+/*  6 : 공 바꾸기
 import java.util.Scanner;
 public class array1_4 {
     public static void main(String[] args) {
@@ -158,4 +158,25 @@ public class array1_4 {
             System.out.print(array[r] + " ");
         }
     }
-}  
+}                   */
+// 7번 : 과제 안 내신 분..?
+import java.util.Scanner;
+public class array1_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int array[] = new int[31];
+
+        for(int s = 0; s < 31; s++) {
+            array[s] = 0;
+        }
+        for(int i = 1; i <= 28; i++) {
+            int n = sc.nextInt();
+            array[n] = n;
+        }
+        for(int i = 1; i <= 30; i++) {
+            if(array[i] == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+} 
