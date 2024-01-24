bd57c935fea0db3eff3f598534c6d542935b2120
zieunn <kje003300@naver.com>
2024-01-25 01:05:09 +0900

    1차원 배열 8번 추가

--- array1_4.java
+++ array1_4.java
@@ -159,7 +159,7 @@ public class array1_4 {
         }
     }
 }                   */
-// 7번 : 과제 안 내신 분..?
+/*  7번 : 과제 안 내신 분..?
 import java.util.Scanner;
 public class array1_4 {
     public static void main(String[] args) {
@@ -179,4 +179,46 @@ public class array1_4 {
             }
         }
     }
-} 
+}           */
+
+/*   8번 : 나머지
+import java.util.Scanner;
+public class array1_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    
+        int array[] = new int [10];
+        int ar[] = new int [10];
+        int count = 0;
+
+        for(int i = 0; i < 10; i++) {
+            int a = sc.nextInt();
+            array[i] = a;
+            ar[i] = a % 42;
+
+            int rmsid = 0;
+        
+            for(int j = 0; j < i; j++) {
+                if(ar[i] == ar[j]) {
+                    rmsid += 1;                    
+                } 
+       
+            } if(rmsid == 0) {
+                count += 1;
+          }     
+        } System.out.println(count);
+    }  
+}       */
+// HashSet 이용
+import java.util.HashSet;
+import java.util.Scanner;
+public class array1_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    
+        HashSet<Integer> h = new HashSet<Integer>();
+
+        for(int i = 0; i < 10; i++) {
+            int a = sc.nextInt();
+            h.add(a % 42);
+        } System.out.println(h.size());
+    }  
+}
