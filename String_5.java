54b272708bd580cb5d2bcaf83ab770533526d947
zieunn <150898812+zieunn@users.noreply.github.com>
2024-02-07 00:04:06 +0900

    문자열 7번 추가

--- String_5.java
+++ String_5.java
@@ -68,7 +68,7 @@ public class String_5 {
     }
 }                            */
 
-// 6번 알파벳 찾기
+/* 6번 알파벳 찾기
 import java.util.Scanner;
 public class String_5 {
     public static void main(String[] args) {
@@ -90,4 +90,28 @@ public class String_5 {
             System.out.print(arr[i] + " ");
         }
     }
-}
+}                       */
+
+/* 7번 문자열 반복
+import java.util.Scanner;
+public class String_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int b = 0; b < t; b++) {
+            int r = sc.nextInt();
+            String s = sc.next();
+            for(int i = 0; i < s.length(); i++) {
+                for(int a = 0; a < r; a++) {
+                    System.out.print(s.charAt(i));
+                }
+            
+            }
+            System.out.println();    
+        }
+    }
+}               */
+
+
+    
