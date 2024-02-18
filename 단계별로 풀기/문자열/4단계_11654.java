9acfe547eeb9c79b01d25876b773096ee38b557e
zieunn <150898812+zieunn@users.noreply.github.com>
2024-02-18 19:41:09 +0900

    11654 추가

---
+++ 단계별로 풀기/문자열/4단계_11654.java
@@ -0,0 +1,10 @@
+//문자열
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int number = s.charAt(0);
+        System.out.println(number);
+    }
+} 
