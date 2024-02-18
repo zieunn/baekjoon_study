42582b0b429b7dc2b629c155b74db67085666d34
zieunn <150898812+zieunn@users.noreply.github.com>
2024-02-18 18:09:07 +0900

    8393 추가

---
+++ 단계별로 풀기/반복문/3단계_8393.java
@@ -0,0 +1,15 @@
+//합 
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int result = 0;
+		for(int i = 0; i <= n; i++) {
+			result += i;
+		}		
+		System.out.println(result);
+		
+    }   
+}      
