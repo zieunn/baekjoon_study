7dadb843bb05ebba5eca4b382fe949220c97b7e1
zieunn <150898812+zieunn@users.noreply.github.com>
2024-02-18 18:10:51 +0900

    10951 추가

---
+++ 단계별로 풀기/반복문/12단계_10951.java
@@ -0,0 +1,16 @@
+//A+B-4 
+import java.util.Scanner;
+public class iteration_3 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+		while(sc.hasNextInt()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(a+b);
+			
+	  }
+	}   
+}	
