a1539f42b4b327e13a46afd8a8b7683ad7397749
zieunn <150898812+zieunn@users.noreply.github.com>
2024-02-18 18:10:10 +0900

    11022 추가

---
+++ 단계별로 풀기/반복문/8단계_11022.java
@@ -0,0 +1,44 @@
+// A+B-8
+//Scanner 이용
+import java.util.Scanner;      
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		String result[] = new String[t];
+
+		for(int i = 0; i<t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			result[i] = "Case #" + (i+1)+ ": "+a+" + "+b+" = "+(a+b);
+		}
+		for(int i = 0; i<t; i++) {
+		System.out.println(result[i]);
+		}
+    }   
+} 	
+
+// 	BufferedReader 이용
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int t = Integer.parseInt(br.readLine());
+		String result[] = new String[t];
+
+		for(int i = 0; i<t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			result[i] = "Case #" + (i+1)+ ": "+a+" + "+b+" = "+(a+b)+"\n";
+		}
+		for(int i = 0; i<t; i++) {
+			bw.write(result[i]);
+		}
+		bw.flush();
+    }   
+}	
