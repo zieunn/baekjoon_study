71ca6e4d852b3a8cc0c71971f28bf483079edeab
zieunn <150898812+zieunn@users.noreply.github.com>
2024-02-18 19:39:13 +0900

    2743

---
+++ 단계별로 풀기/문자열/2단계_2743.java
@@ -0,0 +1,9 @@
+//단어 길이 재기
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s.length());
+    }
+}  
