ca57606e943affe7e30904ed3901cebf163ba32b
zieunn <150898812+zieunn@users.noreply.github.com>
2024-02-18 17:55:19 +0900

    2480 추가

---
+++ 단계별로 풀기/조건문/7단계_2480.java
@@ -0,0 +1,29 @@
+//주사위 세개
+import java.util.Scanner;
+public class Main  {
+	public static void main(String[] args) {		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+if(a==b && b==c && c==a) {
+			System.out.println(10000+(a*1000));
+		}else if ((a==b || b==c) || c==a) {
+			if(a==b || a==c) {
+				System.out.println(1000+(a*100));
+			}else {
+				System.out.println(1000+(b*100));
+			}
+		}else {
+			if(a>b && a>c) {
+				System.out.println(a*100);
+			}else if(b>a && b>c){
+				System.out.println(b*100);
+			}else {
+				System.out.println(c*100);
+			}
+		}
+		
+	}
+
+}
